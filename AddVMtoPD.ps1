82ae042e047880897ca04e5fe3b35e3c90e18238
OptimalZ06 <troy.g.ward@gmail.com>
2018-08-10 09:23:54 -0500

    Update AddVMtoPD.ps1

--- AddVMtoPD.ps1
+++ AddVMtoPD.ps1
@@ -1,1 +1,2 @@
-Get-NTNXVM | where-object {$_.protectionType -Match "unprotected" -and $_.vmName -notlike "*CVM*"} | % { Add-NTNXProtectionDomainVM -Ids $_.vmId -PdName "PD" }
+$PD = "PD_NAME_HERE"
+Get-NTNXVM | where-object {$_.protectionType -Match "unprotected" -and $_.vmName -notlike "*CVM*"} | % { Add-NTNXProtectionDomainVM -Ids $_.vmId -PdName "$PD" }
